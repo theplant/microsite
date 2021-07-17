e0680294542326c5b7daa382739d4f423972851f
han2015 <1019850453@qq.com>
2021-07-17 10:48:30 +0800

    manual actions should update scheduleTime

--- change_status.go
+++ change_status.go
@@ -3,6 +3,7 @@ package microsite
 import (
 	"fmt"
 
+	"github.com/jinzhu/gorm"
 	"github.com/qor/admin"
 )
 
@@ -13,16 +14,29 @@ func ChangeStatus(argument *admin.ActionArgument, action string) (err error) {
 		if version, ok := record.(QorMicroSiteInterface); ok {
 			argument.Context.Result = version
 			status := changeStatusActionMap[action]
+			now := gorm.NowFunc()
 
 			if action == Action_publish {
+				if err = db.Model(version).UpdateColumns(map[string]interface{}{"scheduled_start_at": &now}).Error; err != nil {
+					return
+				}
 				if err = Publish(db, version, true); err != nil {
 					return
 				}
 			} else if action == Action_republish {
+				//have to reset scheduled_end_at
+				if err = db.Model(version).UpdateColumns(map[string]interface{}{"scheduled_start_at": &now, "scheduled_end_at": nil}).Error; err != nil {
+					return
+				}
+
 				if err = Republish(db, version, true); err != nil {
 					return
 				}
 			} else if action == Action_unpublish {
+				if err = db.Model(version).UpdateColumns(map[string]interface{}{"scheduled_end_at": &now}).Error; err != nil {
+					return
+				}
+
 				if err = Unpublish(db, version, true); err != nil {
 					return
 				}
